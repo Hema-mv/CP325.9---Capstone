9b008ca046f26a0fe6ade8640053cbbacc17f617
Hema-mv <mvshema@gmail.com>
2025-01-20 20:34:22 -0600

    Update conn.mjs

--- ecommerce-api/db/conn.mjs
+++ ecommerce-api/db/conn.mjs
@@ -8,10 +8,21 @@ dotenv.config();
 const mongoURL = process.env.ATLAS_URI;
 const db = mongoose.connection;
 
+if (!mongoURL) {
+  console.error('MongoDB connection string is not defined in environment variables');
+  process.exit(1);
+}
+
 // connect to mongo
-mongoose.connect(mongoURL);
-mongoose.connection.once("open", () => {
-  console.log("Connected to mongo");
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+  socketTimeoutMS: 45000 // Increase socket timeout to 45 seconds
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
 });
 
 export default db;
